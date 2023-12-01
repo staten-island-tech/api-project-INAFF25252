992cf70309c27fb161a36acb47505771b16f248c
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2023-12-01 09:25:51 -0500

    2

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+const URL = `https://sv443.net/jokeapi/v2/response`;
+
+async function getData(URL){
+    try{
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log(data);
+    } catch(error){
+      document.querySelector("h1").textContent = `Sorry I cannot find ${response}`
+    }
+}
+getData(URL);
+
+/* const url = `https://sv443.net/jokeapi/v2`;
+async function getData(url){
+    try{
+      const response = await fetch(url);
+      if(response.stats != 200){
+        throw new Error(response);
+      }
+      console.log(response);
+      const data = await response.json();
+      console.log(data);
+      document.querySelector("h1").textContent = data.content;
+      document.querySelector("h2").textContent = data.author;
+    } catch(error){
+      document.querySelector("h1").textContent = `Sorry I cannot find ${response}`
+    }
+} */
+
 /* function greet(name){
   const greetPromise = new Promise(function(resolve,reject){
   resolve(`Hello ${name}`);
@@ -9,18 +39,3 @@ console.log(Aaron);
 Aaron.then((result)=>{
   console.log(result)
 }) */
-
-const url = `https://api.quotable.io/random`;
-async function getData(url){
-    try{
-      const response = await fetch(url);
-      console.log(response);
-      const data = await response.json();
-      console.log(data);
-      document.querySelector("h1").textContent = data.content;
-      document.querySelector("h2").textContent = data.author;
-    } catch(error){
-      
-    }
-}
-getData(url);
