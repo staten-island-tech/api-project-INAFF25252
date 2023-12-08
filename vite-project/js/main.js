83a2eef85eb7cc843a9a59b61b97df57ec14673d
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2023-12-07 23:07:28 -0500

    WHAT TH CORSSSSSSS

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -26,7 +26,69 @@ const DOMSelectors = {
   pointTwo:document.getElementById("pointTwo"), */
 };
 console.log(DOMSelectors)
-const URL = "https://holodex.net/api/v2"
+  
+const URL = "https://pokeapi.co/api/v2/pokemon/"
+//const URL = "https://pokeapi.co/api/v2/{endpoint}/"
+
+async function getData(URL){
+  try{
+    const response = await fetch(URL)
+    if(!response.ok){
+      throw new Error(response);
+    }
+    const data = await response.json();
+    console.log(data);
+
+  } catch(error){
+    console.log("nope")    }
+}
+getData(URL); 
+/* const URL = "https://pokemontcg.io/"
+//const APIKEY = `df7c4485-0a70-4cc1-9e2e-519a4f6b08b9`
+// const URL = `https://sv443.net/jokeapi/v2`;
+//X-APIKEY
+async function getData(URL){
+    try{
+      const response = await fetch(URL,
+        {headers: {
+          "X-Api-Key": "ee5186e1-43e4-4517-9356-5b6d610df291",
+         }
+        });
+  
+        console.log(response);
+   
+    } catch(error){
+      console.log("nope")    }
+}
+getData(URL);  */
+/*const URL = "https://collectionapi.metmuseum.org/public/collection/v1/objects"
+async function getData(URL){
+    try{
+      const response = await fetch(URL)
+    
+      if(response.status != 200){
+        throw new Error(response);
+      }
+      console.log(response);
+      const data = await response.json();
+      console.log(data);
+      document.querySelector(".card-img").textContent = data.primaryImage
+      document.querySelector("h2").textContent = data.type;
+    
+          console.log(response);
+        
+    } catch(error){
+      document.querySelector("h1").textContent = `Sorry I cannot find ${response}`
+    }
+}
+getData(URL); */
+
+//const APIKEY = `df7c4485-0a70-4cc1-9e2e-519a4f6b08b9`
+// const URL = `https://sv443.net/jokeapi/v2`;
+//https://official-joke-api.appspot.com/jokes/random
+//https://musicbrainz.org/ws/2/
+//X-APIKEY
+/* const URL = "https://holodex.net/api/v2"
 //const APIKEY = `df7c4485-0a70-4cc1-9e2e-519a4f6b08b9`
 // const URL = `https://sv443.net/jokeapi/v2`;
 //X-APIKEY
@@ -36,15 +98,22 @@ async function getData(URL){
         {headers: {
           "X-APIKEY": "df7c4485-0a70-4cc1-9e2e-519a4f6b08b9",
          }
-        });
-      const data = await response.json(response.type);
-      console.log(data);
+        })
+         if(response.status != 200){
+            throw new Error(response);
+          }
+          console.log(response);
+          const data = await response.json();
+          console.log(data);
+          document.querySelector("h1").textContent = {headers: {"X-APIKEY": "df7c4485-0a70-4cc1-9e2e-519a4f6b08b9"}}
+          document.querySelector("h2").textContent = data.type;
+        
     } catch(error){
       document.querySelector("h1").textContent = `Sorry I cannot find ${response}`
     }
 }
 getData(URL);
-
+ */
 /* const url = `https://sv443.net/jokeapi/v2`;
 async function getData(url){
     try{
@@ -73,3 +142,25 @@ console.log(Aaron);
 Aaron.then((result)=>{
   console.log(result)
 }) */
+
+/* const URL = "https://holodex.net/api/v2/status=live/"
+//const APIKEY = `df7c4485-0a70-4cc1-9e2e-519a4f6b08b9`
+// const URL = `https://sv443.net/jokeapi/v2`;
+//X-APIKEY
+async function getData(URL){
+    try{
+      const response = await fetch(URL,
+        {headers: {
+          "X-APIKEY": "df7c4485-0a70-4cc1-9e2e-519a4f6b08b9",
+         }
+        });
+  
+        if(!response.ok){
+          throw new Error(response);
+        }
+        console.log(response);
+   
+    } catch(error){
+      console.log("nope")    }
+}
+getData(URL);   */
