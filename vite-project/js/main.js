ee1f7bffe7ed22d5c6c3810bcd6e862161063f8c
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2023-12-19 21:53:54 -0500

    Update main.js

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -20,7 +20,51 @@ async function getData(URL){
 getData(URL);  
 
 
+function clear() {  
+  DOMSelectors.gallery.innerHTML=''
+}
+
 function all(btn, endpoint){
+  btn.addEventListener("click", function(event){
+    event.preventDefault();
+    clear();
+    async function pokemon(){
+      for(let i=1; i<=20; i++){
+        try{
+        const get = await fetch(`${URL}${endpoint}${i}`);
+        const poke = await get.json();
+        console.log(poke);
+        const pokemonList = [poke];
+        const pokeCard = pokemonList.map;
+        console.log(pokeCard)
+        function main(){
+          
+        pokeCard.forEach((item)=>  {
+          DOMSelectors.gallery.insertAdjacentHTML("afterbegin", `
+          <div class="card">
+          <h2>${item.name}</h2>
+          <img src="${item.sprites.front_default}">
+
+          </div>`)})
+        }
+        main();
+  
+
+        }catch(error){
+          console.error(error);}
+
+      }
+    }
+  pokemon();
+})}
+
+
+all(DOMSelectors.form, "pokemon/",)
+all(DOMSelectors.form1, "type/")
+all(DOMSelectors.form2, "berry/")
+all(DOMSelectors.form3, "pokemon-species/")
+
+/* function id(btn, endpoint){
   btn.addEventListener("click", function(event){
     event.preventDefault();
  
@@ -49,12 +93,9 @@ async function pokemon(){
 pokemon();
 })}
 
-
-all(DOMSelectors.form, "pokemon/",)
-all(DOMSelectors.form1, "type")
-all(DOMSelectors.form2, "berry")
-all(DOMSelectors.form3, "pokemon-species")
-
-
+id(DOMSelectors.form, "pokemon/",)
+id(DOMSelectors.form1, "type")
+id(DOMSelectors.form2, "berry")
+id(DOMSelectors.form3, "pokemon-species") */
 //Pokemon name, suggest what they meant if spelled wrong for error
 //Different regional forms, "change theme"
